eed722866ce5a229f2de69cd0f17ebb78c73ea19
Omwah <omwah@towhee.org>
2013-03-08 22:00:30 -0800

    Add initial working version with configurable face searching.

--- src/main/java/com/github/omwah/LinkedBookShelf.java
+++ src/main/java/com/github/omwah/LinkedBookShelf.java
@@ -1,28 +1,45 @@
 package com.github.omwah;
 
+import java.util.ArrayList;
+import java.util.logging.Level;
+import org.bukkit.block.BlockFace;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /*
- * This is the main class of the sample plug-in
+ * This is the main class of the plug-in
  */
 public class LinkedBookShelf extends JavaPlugin {
     /*
-     * This is called when your plug-in is enabled
+     * Loads the plugin
      */
     @Override
     public void onEnable() {
         // save the configuration file
         saveDefaultConfig();
         
-        // Create the SampleListener
-        new LinkedBookShelfListener(this);
+        // Load list of faces to check from config
+        ArrayList<BlockFace> check_faces = new ArrayList<BlockFace>();
+        for(Object list_obj : this.getConfig().getList("check_faces")) {
+            if (list_obj != null && list_obj instanceof String) {
+                String face_str = (String) list_obj;
+                try {
+                    check_faces.add(BlockFace.valueOf(face_str));
+                } catch (IllegalArgumentException ex) {
+                    getLogger().log(Level.SEVERE, "Illegal string of BlockFace in check_faces config value: {0}", face_str);
+                }
+            }
+        }
+        // Make sure some faces are configured
+        if(check_faces.size() == 0) {
+            getLogger().log(Level.SEVERE, "No faces configured for searching!");
+        }
         
-        // set the command executor for sample
-        this.getCommand("sample").setExecutor(new LinkedBookShelfCommandExecutor(this));
+        // Create the SampleListener
+        new LinkedBookShelfListener(this, check_faces);
     }
     
     /*
-     * This is called when your plug-in shuts down
+     * Shuts down plugin
      */
     @Override
     public void onDisable() {
--- src/main/java/com/github/omwah/LinkedBookShelfListener.java
+++ src/main/java/com/github/omwah/LinkedBookShelfListener.java
@@ -1,49 +1,66 @@
 package com.github.omwah;
 
 import java.text.MessageFormat;
+import java.util.List;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.Chest;
 
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 /*
- * This is a sample event listener
+ * Listens for clicks on bookshelves and then searches for chests to open for
+ * the player
  */
 public class LinkedBookShelfListener implements Listener {
     private final LinkedBookShelf plugin;
+    List<BlockFace> check_faces;
 
     /*
      * This listener needs to know about the plugin which it came from
      */
-    public LinkedBookShelfListener(LinkedBookShelf plugin) {
+    public LinkedBookShelfListener(LinkedBookShelf plugin, List<BlockFace> check_faces) {
+        this.plugin = plugin;
+        this.check_faces = check_faces;
+        
         // Register the listener
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
+
+    }
+
+    /*
+     * When a player clicks on a bookshelf, if there is a chest touching
+     * the chest then its inventory is opened to the player
+     */
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        // Quickly ignore everything but a right click on bookshelf
+        Block clicked_block = event.getClickedBlock();
+        if(event.getAction() != Action.RIGHT_CLICK_BLOCK || clicked_block.getType() != Material.BOOKSHELF) {
+            return;
+        }
         
-        this.plugin = plugin;
-    }
+        Block bookshelf = event.getClickedBlock();
+        for (BlockFace face : BlockFace.values()) {
+            // Ignore any face not configured to be searched
+            if(check_faces.contains(face)) {
+                Block test_block = bookshelf.getRelative(face);
 
-    /*
-     * Send the sample message to all players that join
-     */
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        event.getPlayer().sendMessage(this.plugin.getConfig().getString("sample.message"));
-    }
-    
-    /*
-     * Another example of a event handler. This one will give you the name of
-     * the entity you interact with, if it is a Creature it will give you the
-     * creature Id.
-     */
-    @EventHandler
-    public void onPlayerInteract(PlayerInteractEntityEvent event) {
-        final EntityType entityType = event.getRightClicked().getType();
+                if(test_block.getType() == Material.CHEST) {
+                    // Open the chest to the player
+                    event.getPlayer().openInventory(((Chest)test_block.getState()).getBlockInventory());
+                    event.setCancelled(true);
+                    return;
+                }
+            }
+        }
 
-        event.getPlayer().sendMessage(MessageFormat.format(
-                "You interacted with a {0} it has an id of {1}",
-                entityType.getName(),
-                entityType.getTypeId()));
     }
 }
